7f38d0201d580df65b1f0f096ba6e4700a825198
Raj sinha <94060322+r-a-j-s-i-n-h-a@users.noreply.github.com>
2024-03-25 03:17:54 +0530

    Delete backend/routes/planRoutes.js

--- backend/routes/planRoutes.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const planController = require('../controllers/planController');
-
-router.post('/', planController.choosePlan);
-
-module.exports = router;
